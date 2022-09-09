f0f3028701b3ff649560a0a3d0e74dd3a67e3836
Zahid <zahidk.8850@gmail.com>
2022-09-09 17:09:07 +0530

    calendly

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,43 @@
 import React from "react";
+import { InlineWidget } from "react-calendly";
 import styled from "styled-components";
+import { device } from "../../utils/responsiveness";
+import { darkTheme } from "../../utils/ThemeConfig";
 
 function Contact({ theme }) {
-  return <Wrappper>Contact</Wrappper>;
+  return (
+    <Wrappper>
+      <Heading theme={theme}>Schedule a meeting with me</Heading>
+      <InlineWidget
+        url="https://calendly.com/kzahid"
+        styles={{
+          height: "646px",
+          padding: "",
+          margin: "0",
+        }}
+      />
+    </Wrappper>
+  );
 }
 
 export default Contact;
 
 const Wrappper = styled.div`
-  margin: 20px;
-  height: 400px;
+  width: 100%;
+`;
+
+const Heading = styled.div`
+  color: ${(props) =>
+    props.theme === "dark" ? `${darkTheme.lightSlate}` : "#222"};
+  font-size: 40px;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  font-weight: 600;
+  @media ${device.tablet} {
+    font-size: 40px;
+  }
+  @media ${device.mobileL} {
+    font-size: 30px;
+  }
 `;
